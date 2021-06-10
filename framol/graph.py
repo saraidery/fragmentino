b7422afc679987d91ee59bc23d08de45d702f75e
Sarai Dery Folkestad <73849829+saraidery@users.noreply.github.com>
2021-06-10 17:13:05 +0200

    Headers for license (#10)
    * remove license
    
    * adding license and copyright headers

--- framol/graph.py
+++ framol/graph.py
@@ -1,3 +1,10 @@
+#  fragmentino
+#  Copyright (C) 2021 the authors of fragmentino
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import numpy as np
 from copy import deepcopy
 
